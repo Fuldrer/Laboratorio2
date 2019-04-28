e8524a428319b95201aae6910d6d130199071038
Carlos <carlosmolina1997@hotmail.com>
2019-04-27 22:04:47 -0600

    Trabajo2

---
+++ src/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Link } from 'react-router-dom'
+
+class Contact extends React.Component{
+    render(){
+        return(
+            <div>
+                <label>
+                    Contact
+                </label>
+            </div>
+        )
+    }
+}
+
+export default Contact;
---
+++ src/User.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Link } from 'react-router-dom'
+
+class User extends React.Component{
+    render(){
+        return(
+            <div>
+                <label>
+                    Users
+                </label>
+            </div>
+        )
+    }
+}
+
+export default User;
